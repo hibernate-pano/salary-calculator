d0e8a83fb3b186603046f5969e3cec9669aa4e61
panbo <panbo.coding@qq.com>
2025-03-14 09:29:13 +0800

    Initial Commit

---
+++ salary-calculator/salary_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  salary_calculatorApp.swift
+//  salary-calculator
+//
+//  Created by panbo on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct salary_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ salary-calculatorTests/salary_calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  salary_calculatorTests.swift
+//  salary-calculatorTests
+//
+//  Created by panbo on 2025/3/14.
+//
+
+import Testing
+@testable import salary_calculator
+
+struct salary_calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
